188732d9ff747b29b7fb2834b8f72847a9872e41
Federico Gabriel Gutierrez <gutierrezfedericog@gmail.com>
2024-10-18 15:32:19 -0300

    PreEntrega1-Gutierrez

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import mocksRouter from "./routes/mocks.router.js"; 
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
@@ -9,7 +10,7 @@ import sessionsRouter from './routes/sessions.router.js';
 
 const app = express();
 const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`URL DE MONGO`)
+const connection = mongoose.connect(`mongodb+srv://gutierrezfedericog:ZGu2Q70OsMrmWJL9@cluster0.yex3ufx.mongodb.net/backend3?retryWrites=true&w=majority&appName=Cluster0`)
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,5 +19,6 @@ app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
 app.use('/api/sessions',sessionsRouter);
+app.use("/api/mocks", mocksRouter); 
 
 app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
